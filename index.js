8ae242eb7fab2826f42c4b8c86e577000364edd0
Tasleemofx <oladepotesleem5@gmail.com>
2021-09-19 09:29:27 +0100

    exercise 3.7-3.8

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require('express')
 let contacts = require('./contacts')
+const morgan = require('morgan')
 const app = express();
 app.use(express.json())
+app.use(morgan('tiny'))
 
 
 app.get("/info",(request, response)=>{
